f711fe80bce67dd8fdc974706939996bc297e165
zhangyanli1 <zhangyanli1@vipkid.com.cn>
2020-11-24 14:11:07 +0800

    加入一些文档以及vue3的资料

---
+++ js/ArraryList.ts
@@ -0,0 +1,28 @@
+/**
+ * name
+ */
+import list from './List'
+class ArrayList extends list{
+    constructor() {
+        super()
+    }
+
+    private size: number  // 下标
+    private _defaultSize : 10,
+    private elementData: any
+    // 数组的初始化【默认长度创建/或者给一个初始值】
+    public arrayList() {
+
+    }
+
+    public sizes() {
+
+    }
+    // 确保容量是否还够
+    public add() {
+
+    }
+    // 扩容机制： 按照旧数组的大小创建一个新数组，将旧数组拷贝到新数组
+    arraryCopy() {}
+
+}
